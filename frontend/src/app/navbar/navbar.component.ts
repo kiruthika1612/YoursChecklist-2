fa2570caf3560331cdad444f995596a04539d9a2
kiruthika1612 <56331587+kiruthika1612@users.noreply.github.com>
2020-04-22 14:33:00 -0400

    commiting navigation bar component files

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+
+
+
+  constructor(private customerSvc: CustomerService, private router: Router) { }
+
+  ngOnInit() {
+
+  }
+
+  toggleLoginState() {
+    this.customerSvc.userLoginInfo.userLogin = !this.customerSvc.userLoginInfo.userLogin;
+  }
+
+  takeMeToDashboard() {
+    var customerName = this.customerSvc.userLoginInfo.cName;
+    this.router.navigateByUrl('/dashboard/' + customerName);
+  }
+}
