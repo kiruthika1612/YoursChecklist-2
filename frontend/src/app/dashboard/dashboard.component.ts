919733a491532a16d12a68dcf04f5c376aafbe92
kiruthika1612 <56331587+kiruthika1612@users.noreply.github.com>
2020-04-22 14:27:08 -0400

    commiting dashboard components

---
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(private custSvc: CustomerService) { }
+
+  myProducts: any[] = undefined;
+  ngOnInit() {
+    this.getMyProducts();
+  }
+
+  getMyProducts() {
+    var data = {
+      cid: this.custSvc.userLoginInfo.cid
+    }
+    this.custSvc.fetchMyProducts(data).subscribe({
+      next: data => {
+        this.myProducts = data.products;
+        console.log(this.myProducts)
+      }
+    })
+  }
+}
